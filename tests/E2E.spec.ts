16c40514251c062b2f53bc04d564c8a27381bb96
Aleksandar Bajic <bajic89aleksandar.bps@gmail.com>
2026-01-31 01:24:48 +0100

    novi

--- tests/E2E.spec.ts
+++ tests/E2E.spec.ts
@@ -1,10 +1,11 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../Pages/LoginPage';
 
-test.describe('Login', () => {
+test.describe('Logged in user tests', () => {
 
     let userEmail = 'Sakismafia122@mail.com';
     let userPassword = 'Password123!';
+    let username = 'Sakimafia122';
 
     test.beforeEach(async ({page}) => {
         const loginPage = new LoginPage(page);
@@ -19,7 +20,38 @@ test.describe('Login', () => {
     });
 
     test('Profile is visible', async ({page}) => {
-        await expect(page.getByRole('link', {name: 'Sakimafia122'})).toBeVisible();
+        await expect(page.getByRole('link', {name: username})).toBeVisible();
+    })
+
+    test('My feed', async ({page}) => {
+        await page.getByText(' Your Feed ').click()
+
+        await expect(page.getByText('No articles are here... yet.')).toBeVisible();
+    })
+
+    test('Logout and login with wrong password', async ({page}) => {
+        await page.getByRole('link', {name: username}).click();
+
+        await expect(page.locator('a', {hasText: ' Edit Profile Settings '})).toBeVisible();
+
+        await page.locator('a', {hasText: ' Edit Profile Settings '}).click();
+
+        await expect(page).toHaveURL('https://conduit.bondaracademy.com/settings');
+
+        //Logout
+        await page.getByRole('button', {name: ' Or click here to logout. '}).click();
+
+        await expect(page.locator('a', {hasText: ' Sign in '})).toBeVisible();
+
+        //SignIn page
+        await page.locator('a', {hasText: ' Sign in '}).click();
+        await expect(page).toHaveURL('https://conduit.bondaracademy.com/login');
+
+        const signinPage = new LoginPage(page);
+        await signinPage.login('wrong@mail.com', 'wrong123')
+        await page.getByRole('button', {name: 'Sign in'}).click();
+
+        await expect(page.locator('li', {hasText: 'email or password is invalid'})).toBeVisible();
     })
 
 });
